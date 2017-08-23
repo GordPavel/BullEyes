cd4900296edb84c74dfb100bf4df5b0ef7ec4bd8
Pavel Gordeev <pvgord@gmail.com>
2017-08-23 13:41:59 +0400

    Made perfect slider label that show slider pointer position. Have to know the slider pointer width

--- bullseyes/ViewController.swift
+++ bullseyes/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         goalNumber = randomGenerator()
         goalNumberLabel!.text = "Hit the \(goalNumber)!"
+        
+        sliderLabel.text = "\( Int( roundf( slider.value ) ) )"
+        let leftMove = slider.frame.minX
+        let allRange = ( slider.frame.width - sliderPointWidth ) * CGFloat( slider.value / 100 )
+        let middleOfSliferLabel = sliderLabel.frame.width / 2
+        let x = leftMove + sliderPointWidth / 2 + allRange - middleOfSliferLabel
+        sliderLabel.frame.origin = CGPoint( x: CGFloat(x) , y: slider.frame.midY - sliderLabel.frame.height / 2 )
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,45 +36,48 @@ class ViewController: UIViewController {
     var totalScore = 0
     var goalNumber = 0
     
-    @IBOutlet weak var slider : UISlider?
-    @IBOutlet weak var goalNumberLabel : UILabel?
-    @IBOutlet weak var roundScoreLabel : UILabel?
-    @IBOutlet weak var totalScoreLabel : UILabel?
-    @IBOutlet weak var roundNumberLabel : UILabel?
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var goalNumberLabel : UILabel!
+    @IBOutlet weak var roundScoreLabel : UILabel!
+    @IBOutlet weak var totalScoreLabel : UILabel!
+    @IBOutlet weak var roundNumberLabel : UILabel!
     
     @IBAction func game(){
-        roundScore = abs( goalNumber - Int( roundf( slider!.value ) ) )
+        roundScore = abs( goalNumber - Int( roundf( slider.value ) ) )
+        self.roundScoreLabel.text = "Round score: \(self.roundScore)"
         
-        let alert = UIAlertController(title: "Round!", message: "GoalNumber: \(goalNumber)\n" +
-            "You scored: \( roundf( slider!.value ) )\n" +
-            "RoundScore: \( roundScore )", preferredStyle: .alert )
-        let action = UIAlertAction(title: "Ok", style: .default ){ ( _ ) in
-            self.roundScoreLabel!.text = "Round score: \(self.roundScore)"
+        self.totalScore += self.roundScore
+        self.totalScoreLabel.text = "Total score: \(self.totalScore)"
+        
+        self.roundNumber += 1
+        self.roundNumberLabel.text = "Round number: \(self.roundNumber)"
             
-            self.totalScore += self.roundScore
-            self.totalScoreLabel!.text = "Total score: \(self.totalScore)"
-            
-            self.roundNumber += 1
-            self.roundNumberLabel!.text = "Round number: \(self.roundNumber)"
-            
-            self.goalNumber = self.randomGenerator()
-            self.goalNumberLabel!.text = "Hit the \(self.goalNumber)!"
-        }
-        alert.addAction( action )
-        self.present( alert, animated: true )
+        self.goalNumber = self.randomGenerator()
+        self.goalNumberLabel.text = "Hit the \(self.goalNumber)!"
+        
     }
     
     @IBAction func newGame(){
         goalNumber = randomGenerator()
-        goalNumberLabel!.text = "Hit the \(goalNumber)!"
+        goalNumberLabel.text = "Hit the \(goalNumber)!"
         
-        slider!.value = 50.0
         roundNumber = 1
         totalScore = 0
         
-        roundScoreLabel!.text = "Round score: "
-        totalScoreLabel!.text = "Total score: 0"
-        roundNumberLabel!.text = "Round number: 1"
+        roundScoreLabel.text = "Round score: "
+        totalScoreLabel.text = "Total score: 0"
+        roundNumberLabel.text = "Round number: 1"
+    }
+    
+    var sliderPointWidth : CGFloat = 32.0
+    @IBAction func sliderValueChanged( _ slider : UISlider ){
+        sliderLabel.text = "\( Int( roundf( slider.value ) ) )"
+        let leftMove = slider.frame.minX
+        let allRange = ( slider.frame.width - sliderPointWidth ) * CGFloat( slider.value / slider.maximumValue )
+        let middleOfSliferLabel = sliderLabel.frame.width / 2
+        let x = leftMove + sliderPointWidth / 2 + allRange - middleOfSliferLabel
+        sliderLabel.frame.origin = CGPoint( x: CGFloat(x) , y: sliderLabel.frame.minY )
     }
 }
 
