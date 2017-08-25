d380d50b4ad0033df08c5b9aa77310d9fe5a2424
Pavel Gordeev <pvgord@gmail.com>
2017-08-25 12:19:11 +0400

    slider label works perfect, made resizable interface, now you can play even om iPad 12.9

--- bullseyes/ViewController.swift
+++ bullseyes/ViewController.swift
@@ -16,12 +16,9 @@ class ViewController: UIViewController {
         goalNumber = randomGenerator()
         goalNumberLabel!.text = "Hit the \(goalNumber)!"
         
-        sliderLabel.text = "\( Int( roundf( slider.value ) ) )"
-        let leftMove = slider.frame.minX
-        let allRange = ( slider.frame.width - sliderPointWidth ) * CGFloat( slider.value / 100 )
-        let middleOfSliferLabel = sliderLabel.frame.width / 2
-        let x = leftMove + sliderPointWidth / 2 + allRange - middleOfSliferLabel
-        sliderLabel.frame.origin = CGPoint( x: CGFloat(x) , y: slider.frame.midY - sliderLabel.frame.height / 2 )
+        roundScoreLabel.text = "Round score: \( roundScore )"
+        totalScoreLabel.text = "Total score: \( totalScore )"
+        roundNumberLabel.text = "Round number \( roundNumber )"
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,14 +33,28 @@ class ViewController: UIViewController {
     var totalScore = 0
     var goalNumber = 0
     
-    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UILabel!{
+        didSet{
+            sliderValueChanged()
+            sliderLabel.isHidden = true
+        }
+    }
     @IBOutlet weak var slider : UISlider!
     @IBOutlet weak var goalNumberLabel : UILabel!
     @IBOutlet weak var roundScoreLabel : UILabel!
     @IBOutlet weak var totalScoreLabel : UILabel!
     @IBOutlet weak var roundNumberLabel : UILabel!
     
+    var godMode = false
+    @IBAction func godMode(_ sender: UIButton) {
+        godMode = !godMode
+        sliderLabel.isHidden = !godMode
+        let underLined = godMode ? 1 : 0
+        sender.setAttributedTitle( NSAttributedString(string: "God mode", attributes: [ NSUnderlineStyleAttributeName : underLined ] ), for: .normal )
+    }
     @IBAction func game(){
+        sliderLabel.isHidden = false
+        
         roundScore = abs( goalNumber - Int( roundf( slider.value ) ) )
         self.roundScoreLabel.text = "Round score: \(self.roundScore)"
         
@@ -70,9 +81,20 @@ class ViewController: UIViewController {
         roundNumberLabel.text = "Round number: 1"
     }
     
-    var sliderPointWidth : CGFloat = 32.0
-    @IBAction func sliderValueChanged( _ slider : UISlider ){
+    @IBAction func info() {
+        let alert = UIAlertController(title: "Tutorial", message: "You have to set on slider value, that label over it shows.", preferredStyle: .alert )
+        let understandAction = UIAlertAction(title: "Understand", style: .default )
+        alert.addAction( understandAction )
+        present( alert , animated: true ) 
+    }
+    
+    @IBAction func sliderValueChanged(){
+        if !godMode{
+            sliderLabel.isHidden = true
+        }
+        
         sliderLabel.text = "\( Int( roundf( slider.value ) ) )"
+        let sliderPointWidth = slider.thumbRect( forBounds: slider.bounds, trackRect: slider.trackRect( forBounds: slider.bounds ), value: 0 ).size.width
         let leftMove = slider.frame.minX
         let allRange = ( slider.frame.width - sliderPointWidth ) * CGFloat( slider.value / slider.maximumValue )
         let middleOfSliferLabel = sliderLabel.frame.width / 2
